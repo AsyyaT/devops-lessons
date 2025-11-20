ac6be2f6958558526e04488d6521b8f67c48f9a6
anastasiia <aizatsana15@gmail.com>
2025-11-20 18:18:15 +0200

    added jenkins settings

--- main.tf
+++ main.tf
@@ -26,3 +26,59 @@ module "eks" {
   private_subnet_ids = module.vpc.private_subnets
 }
 
+data "aws_eks_cluster" "eks" {
+  name       = module.eks.eks_cluster_name
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name       = module.eks.eks_cluster_name
+  depends_on = [module.eks]
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
+
+
+module "jenkins" {
+  source             = "./modules/jenkins"
+  cluster_name       = module.eks.eks_cluster_name
+  oidc_provider_arn  = module.eks.oidc_provider_arn
+  oidc_provider_url  = module.eks.oidc_provider_url
+  github_pat         = var.github_pat
+  github_user        = var.github_user
+  github_repo_url     = var.github_repo_url
+  ecr_repository_url  = module.ecr.repository_url
+  depends_on         = [module.eks]
+  providers          = {
+    helm       = helm
+    kubernetes = kubernetes
+  }
+}
+
+module "argo_cd" {
+  source            = "./modules/argo_cd"
+  namespace         = "argocd"
+  chart_version     = "5.46.4"
+  github_repo_url   = var.github_repo_url
+  github_user       = var.github_user
+  github_pat        = var.github_pat
+  app_target_revision = "lesson-8-9"
+  depends_on        = [module.eks]
+  providers         = {
+    helm       = helm
+    kubernetes = kubernetes
+  }
+
+
