4cb8428a5f2c41592baa32c1e2b1efd82eb9d391
anastasiia <aizatsana15@gmail.com>
2025-11-20 19:02:00 +0200

    Added django app

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
-FROM python:3.11-slim
+FROM python:3.12-slim
+
+ENV PYTHONDONTWRITEBYTECODE=1 \
+    PYTHONUNBUFFERED=1 \
+    PIP_NO_CACHE_DIR=1
 
 WORKDIR /app
 
@@ -10,6 +14,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-ENV PYTHONUNBUFFERED=1
+RUN python manage.py collectstatic --noinput || true
 
 CMD ["gunicorn", "app.wsgi:application", "--bind", "0.0.0.0:8000"]
--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%2p4$ynb!z9kd73_t+!scnn5ye%a8f5^pm)c3039zazu7shls^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hello'
 ]
 
 MIDDLEWARE = [
